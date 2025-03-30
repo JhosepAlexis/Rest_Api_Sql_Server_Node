720e279fdd3d6ca384eaef4c8edca944ebc47491
JhosepAlexis <j3.alexischavarro@gmail.com>
2025-03-29 22:20:58 -0500

    update latlon.controllers.js

--- src/controllers/latlon.controllers.js
+++ src/controllers/latlon.controllers.js
@@ -2,6 +2,6 @@ import { getConnection } from "../databases/connection.js";
 
 export const getLatitudLongitud = async (req, res) => {
   const pool = await getConnection();
-  const result = await pool.request().query("SELECT Latitud, Longitud FROM AdmTerceros");
+  const result = await pool.request().query("SELECT Latitud, Longitud FROM AdmTerceros WHERE Latitud IS NOT NULL AND Longitud IS NOT NULL");
   res.json(result.recordset);
 };
