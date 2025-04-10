1142468a57f600994f0b0ad9d73dc17adf2802d8
JhosepAlexis <j3.alexischavarro@gmail.com>
2025-04-09 23:24:00 -0500

    update customerlistseller.controller

--- src/controllers/customerlistseller.controllers.js
+++ src/controllers/customerlistseller.controllers.js
@@ -3,9 +3,10 @@ import sql from "mssql";
 
 
 export const getCustomerListSeller = async (req, res) => {
-  const { VendedorID } = req.body;
+  const { VendedorID } = req.params;
   const pool = await getConnection();
-  const result = await pool.request()
+  const result = await pool
+  .request()
   .input('VendedorID', sql.Int, VendedorID)
   .query("SELECT TercerosIdentificacion, TercerosNombres, TercerosDireccion, 'home' as type, Latitud, Longitud FROM AdmTerceros WHERE VendedorID=@VendedorID AND Latitud IS NOT NULL AND Longitud IS NOT NULL");
   res.json(result.recordset);
