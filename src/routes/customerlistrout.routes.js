a6752950ad80f4dcf12b09c1dc0d17746257ebbd
JhosepAlexis <j3.alexischavarro@gmail.com>
2025-04-21 09:21:03 -0500

    update customerlistrout.routes.js

--- src/routes/customerlistrout.routes.js
+++ src/routes/customerlistrout.routes.js
@@ -5,6 +5,6 @@ import {
 
 const router = Router();
 
-router.get("/listadoclientesrutero", getCustomerListRout);
+router.post("/listadoclientesrutero", getCustomerListRout);
 
 export default router;   
