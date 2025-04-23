611e26a7c27b438d2cc83e5daa9c36e2caf1e4e2
JhosepAlexis <j3.alexischavarro@gmail.com>
2025-04-23 11:24:39 -0500

    update seller.controller

--- src/controllers/seller.controllers.js
+++ src/controllers/seller.controllers.js
@@ -52,20 +52,7 @@ export const login = async (req, res) => {
 
       });
     }
-    
-    // if (Clave !== user.Clave) {
-    //   return res.status(401).json({
-    //     success: false,
-    //     message: 'Credenciales inválidas'
-    //   });
-    // }
 
-    // 4. Verificar la contraseña
-    // Nota: Esto asume que estás almacenando las contraseñas con hash usando bcrypt
-    // Si no es así, deberás adaptar esta parte según tu método de almacenamiento
-    // const passwordMatch = await bcrypt.compare(Clave, user.Clave);
-    
-    // Si no usas bcrypt, puedes hacer una comparación directa (no recomendado para producción)
     const passwordMatch = (Clave === user.Clave);
     
     if (!passwordMatch) {
