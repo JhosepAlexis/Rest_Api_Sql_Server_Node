bfa19c732d8859b97d147223635f1e7823f8fde9
JhosepAlexis <j3.alexischavarro@gmail.com>
2025-04-23 01:42:35 -0500

    update customerlistrout.controller Estado

--- src/controllers/customerlistrout.controllers.js
+++ src/controllers/customerlistrout.controllers.js
@@ -7,6 +7,34 @@ export const getCustomerListRout = async (req, res) => {
   const result = await pool.request()
   .input('VendedorID', sql.Int, VendedorID)
   .input('Dia', sql.NVarChar, Dia)
-  .query("SELECT RUT.TercerosID, RUT.Cedula, RUT.TercerosNombres, RUT.Direccion, 'home' as type, TER.Latitud, TER.Longitud FROM RuteroVendedores RUT INNER JOIN AdmTerceros TER ON TER.TercerosID=RUT.TercerosID WHERE RUT.VendedorID=@VendedorID AND RUT.Dia=@Dia");
+  .query(`
+    SELECT 
+      RUT.TercerosID,
+      RUT.Cedula,
+      RUT.TercerosNombres, 
+      RUT.Direccion, 
+      'home' as type, 
+      TER.Latitud, 
+      TER.Longitud,
+      COALESCE(
+          (SELECT TOP 1 'VENTA' 
+           FROM InvNo_Venta_Pedidos 
+           WHERE TercerosID = RUT.TercerosID 
+           AND CONVERT(DATE, Fecha) = CONVERT(DATE, GETDATE())
+           AND Detalle = ''),
+          (SELECT TOP 1 'NO-VENTA' 
+           FROM InvNo_Venta_Pedidos 
+           WHERE TercerosID = RUT.TercerosID 
+           AND CONVERT(DATE, Fecha) = CONVERT(DATE, GETDATE())
+           AND Detalle <> ''),
+          'PENDIENTE'
+      ) AS Estado
+    FROM 
+      RuteroVendedores RUT INNER JOIN 
+      AdmTerceros TER ON TER.TercerosID=RUT.TercerosID 
+    WHERE 
+      RUT.VendedorID=@VendedorID AND RUT.Dia=@Dia
+    `
+  );
   res.json(result.recordset);
 };
